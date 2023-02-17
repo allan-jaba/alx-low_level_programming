c0c38dcb401569520183309b9882e3f4ab82fc2d
Allan Baleke <allan.baleke1@gmail.com>
2023-02-17 07:48:31 -0800

    numbers of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+
+	/* Get the character to be written*/
+	char ch = '1';
+
+	/* Write the Character to stdout*/
+	for (ch = '1'; ch <= '9'; ch++)
+		putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
+

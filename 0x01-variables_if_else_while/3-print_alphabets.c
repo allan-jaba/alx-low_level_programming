70237d23971f3fcf9981f4513aa5b4d04d795fca
Allan Baleke <allan.baleke1@gmail.com>
2023-02-17 06:41:12 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main()
+{
+    char i;
+    for(i='a'; i<='z'; i++)
+        putchar(i);
+   
+    for(i='A'; i<='Z'; i++)
+       
+        putchar(i);
+  
+   
+    return 0;
+}
+
